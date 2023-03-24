ab8dec32a31f3af4b52882113fa6e9eef93a120e
sumurillo <sumire.ohana@gmail.com>
2023-03-24 16:05:18 -0500

    end of assessment

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.db import models
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def ItemCreate(request):
+    return render(request, 'create.html')
+
+# class ItemList(ListView): 
+#     model = Item
+
+# class ItemDelete(DeleteView):
+#     model = Item
+#     def get_success_url(self):
+#         return reverse('', args=[self.object.item.id])
